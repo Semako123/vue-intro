227d99490539287a7bdb2739dcfcf13e0c48e7a2
semako123 <zosusemenoch@gmail.com>
2023-08-05 17:36:30 +0100

    Project inti

---
+++ vue-intro/src/App.vue
@@ -0,0 +1,21 @@
+<script setup>
+import { ref } from "vue";
+const count = ref(0);
+
+const add = () => {
+  count.value++;
+};
+
+const sub = () => {
+  count.value--;
+};
+</script>
+
+<template>
+  <div>{{ count }}</div>
+  <button @click="add">+</button>
+  <button @click="sub">-</button>
+</template>
+
+<style scoped>
+</style>
